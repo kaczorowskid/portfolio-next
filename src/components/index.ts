b426002b243c676443d2ba026e7f320e7ce52508
Damian Kaczorowski <kaczorowskid97@gmail.com>
2025-10-20 19:12:30 +0200

    refactor: remove container component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
 export { Badge, Button, Input, Label, List, Textarea } from "./ui";
 export { MainLayout } from "./main-layout";
-export { Container } from "./container";
 export { PageSection } from "./page-section";
