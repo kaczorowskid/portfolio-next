f5106eea273cbd51c358f36a5a3dd21bb24ca432
Damian Kaczorowski <kaczorowskid97@gmail.com>
2026-06-14 21:05:07 +0200

    hotfix: change max width

--- src/components/main-layout/main-layout-bar/main-layout-bar.tsx
+++ src/components/main-layout/main-layout-bar/main-layout-bar.tsx
@@ -2,7 +2,7 @@ import { MainLayoutBarIndicator } from "./main-layout-bar-indicator";
 
 export const MainLayoutBar = () => (
   <div className="w-full fixed z-50">
-    <div className="mx-auto max-w-350 flex justify-center bg-black border border-r-green-600 border-l-green-600 border-b-green-600 rounded-bl-2xl rounded-br-2xl">
+    <div className="mx-auto max-w-318 flex justify-center bg-black border border-r-green-600 border-l-green-600 border-b-green-600 rounded-bl-2xl rounded-br-2xl">
       <MainLayoutBarIndicator />
     </div>
   </div>
